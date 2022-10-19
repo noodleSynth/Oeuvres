bf00704d30c51dca7480d3cc6ed1f487f6debf47
William Gourley <williamgourley@Williams-Pancake.local>
2022-10-19 18:25:41 +0200

    Nevermind the server

--- srv/routes/api.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-const { getMarkdown } = require('../service/github.service')
-
-router.get('/markdown/:owner/:repo/:path(*)', async (req, res, next) => {
-  res.send(await getMarkdown(req.params))
-  // res.send(req.path)
-})
-
-module.exports = router
